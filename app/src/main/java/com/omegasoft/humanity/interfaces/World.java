5e7b206b18e250cd0520d3816ee4b8bd8b270889
Farhad <omegasoft7@gmail.com>
2016-02-12 14:17:50 +0200

    make world 3D

--- app/src/main/java/com/omegasoft/humanity/interfaces/World.java
+++ app/src/main/java/com/omegasoft/humanity/interfaces/World.java
@@ -9,21 +9,25 @@ public abstract class World {
 
     private float startX = 0;
     private float startY = 0;
+    private float startZ = 0;
 
     private float finishX = 0;
     private float finishY = 0;
+    private float finishZ = 0;
 
     ArrayList<ObjectInWorld> objecstInWorld = new ArrayList<>();
 
-    public World(float startx, float starty, float finishx, float finishy) {
+    public World(float startx, float starty, float startz,float finishx, float finishy, float finishz) {
         this.startX = startx;
         this.startY = starty;
+        this.startZ = startz;
         this.finishX = finishx;
         this.finishY = finishy;
+        this.finishZ = finishz;
     }
 
-    public World(float finishx, float finishy) {
-        this(0, 0, finishx, finishy);
+    public World(float finishx, float finishy, float finishz) {
+        this(0, 0, 0, finishx, finishy, finishz);
     }
 
     public boolean attachObjectToWorld(ObjectInWorld objectInWorld) {
